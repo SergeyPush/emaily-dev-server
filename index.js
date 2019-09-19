0823075bace6ecf6ba5a46c9618f2a5a13864cb9
Sergii Pushkovskyi <Sergii.Pushkovskyi@infopulse.com>
2019-09-19 09:42:20 +0300

    Added Google OAuth

--- index.js
+++ index.js
@@ -2,15 +2,33 @@ const express = require("express");
 const morgan = require("morgan");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
+app.use(morgan("combined"));
 
 // Client ID: 948465709883-qs68vmg00jutte5mo166ceae12cu57ks.apps.googleusercontent.com
 // Client Secret: _qWzWv1SygCA_snCoKoVKiuV
 
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
-app.use(morgan("combined"));
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
 
 app.get("/", (req, res) => {
   res.status(200).json({
